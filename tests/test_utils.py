69cb647b6d359c36d7312475f2b542ee25c456f0
ttw225 <peter86225@gmail.com>
2021-11-01 23:14:26 +0800

    test(tests/test_utils.py): update test_get_project_sign

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,3 +1,6 @@
+import os
+
+import git
 import pytest
 
 from validator.config import GPG_SIGN
@@ -11,5 +14,17 @@ def import_sign_keys(key_files=["example_private.pgp", "example2_private.pgp"]):
             GPG_SIGN.import_keys(key_fh.read())
 
 
-def test_get_project_sign():
-    assert get_project_sign() == "68A8DC40153D6B6F"
+@pytest.fixture
+def backup_git_key():
+    original_key: str = str(git.Repo(os.getcwd()).config_reader().get_value("user", "signingkey"))
+    # Set to empty
+    git.Repo(os.getcwd()).config_writer().set_value("user", "signingkey", "").release()
+    yield
+    # Restore
+    git.Repo(os.getcwd()).config_writer().set_value("user", "signingkey", original_key).release()
+
+
+@pytest.mark.parametrize("key_id", [(""), ("4BDCFAF35C34FA95"), ("32C5B310A13AFADA")])
+def test_get_project_sign(backup_git_key, key_id: str):
+    git.Repo(os.getcwd()).config_writer().set_value("user", "signingkey", key_id).release()
+    assert get_project_sign() == key_id
