bef2930c6ac4dbc5bda4a7a8787d52365285cdd4
ttw225 <peter86225@gmail.com>
2021-11-02 23:55:03 +0800

    test(tests/test_utils.py): add import_sign_keys fixture

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 
 import git
 import pytest
@@ -7,11 +8,29 @@ from validator.config import GPG_SIGN
 from validator.utils import get_project_sign
 
 
+TEST_KEYS = [
+    {
+        "file": "example_private.pgp",
+        "fingerprint": "7D210A94B74DD63A4C3C64254BDCFAF35C34FA95",
+        "passphrase": "very example p@ssword",
+    },
+    {
+        "file": "example2_private.pgp",
+        "fingerprint": "25178EFFE362212489FDA16F32C5B310A13AFADA",
+        "passphrase": "very example2 p@ssw0rd",
+    },
+]
+
+
 @pytest.fixture
-def import_sign_keys(key_files=["example_private.pgp", "example2_private.pgp"]):
+def import_sign_keys(key_files=TEST_KEYS):
     for key_file in key_files:
-        with open(f"tests/sample_keys/{key_file}", "rb") as key_fh:
+        with open(f"tests/sample_keys/{key_file['file']}", "rb") as key_fh:
             GPG_SIGN.import_keys(key_fh.read())
+    yield
+    for key_file in key_files:
+        GPG_SIGN.delete_keys(key_file["fingerprint"], secret=True, passphrase=key_file["passphrase"])
+        GPG_SIGN.delete_keys(key_file["fingerprint"], secret=False, passphrase=key_file["passphrase"])
 
 
 @pytest.fixture
@@ -28,3 +47,7 @@ def backup_git_key():
 def test_get_project_sign(backup_git_key, key_id: str):
     git.Repo(os.getcwd()).config_writer().set_value("user", "signingkey", key_id).release()
     assert get_project_sign() == key_id
+
+
+def test_import_sign(import_sign_keys):
+    assert len(GPG_SIGN.list_keys()) == len(TEST_KEYS)
