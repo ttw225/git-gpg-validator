520aa69a3600618f94a9cdb174f0d2e4c67f9a14
ttw225 <peter86225@gmail.com>
2021-11-03 19:02:53 +0800

    test(tests/config.py): move keys to config.py

---
+++ tests/config.py
@@ -0,0 +1,12 @@
+TEST_KEYS = [
+    {
+        "file": "example_private.pgp",
+        "fingerprint": "7D210A94B74DD63A4C3C64254BDCFAF35C34FA95",
+        "passphrase": "very example p@ssword",
+    },
+    {
+        "file": "example2_private.pgp",
+        "fingerprint": "25178EFFE362212489FDA16F32C5B310A13AFADA",
+        "passphrase": "very example2 p@ssw0rd",
+    },
+]
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,19 +7,7 @@ import pytest
 from validator.config import GPG_SIGN
 from validator.utils import get_project_sign
 
-
-TEST_KEYS = [
-    {
-        "file": "example_private.pgp",
-        "fingerprint": "7D210A94B74DD63A4C3C64254BDCFAF35C34FA95",
-        "passphrase": "very example p@ssword",
-    },
-    {
-        "file": "example2_private.pgp",
-        "fingerprint": "25178EFFE362212489FDA16F32C5B310A13AFADA",
-        "passphrase": "very example2 p@ssw0rd",
-    },
-]
+from .config import TEST_KEYS
 
 
 @pytest.fixture
