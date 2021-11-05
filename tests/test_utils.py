e8f9725e95c6c12d2ba675933385f59a649e6044
ttw225 <peter86225@gmail.com>
2021-11-05 20:12:31 +0800

    test(tests/test_utils.py): add test_get_github_gpgs

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,9 +3,10 @@ from typing import List
 
 import git
 import pytest
+import requests
 
-from validator.config import GPG_SIGN
-from validator.utils import get_project_sign, parse_github_response
+from validator.config import GITHUB_GPG_URI, GITHUB_HEADERS, GPG_SIGN
+from validator.utils import get_github_gpgs, get_project_sign, parse_github_response
 
 from .config import TEST_KEYS
 
@@ -26,56 +27,68 @@ def test_get_project_sign(backup_git_key, key_id: str):
     assert get_project_sign() == key_id
 
 
+GITHUB_RESPONSE = [
+    ([], []),
+    (
+        [
+            {
+                "key_id": "KEY-ID-OK",
+                "raw_key": "RAW-KEY-OK",
+                "can_certify": True,
+            }
+        ],
+        [("KEY-ID-OK", "RAW-KEY-OK")],
+    ),
+    (
+        [
+            {
+                "key_id": "KEY-ID-OK",
+                "raw_key": "RAW-KEY-OK",
+                "can_certify": True,
+            },
+            {
+                "key_id": "KEY-ID-BAD",
+                "raw_key": "RAW-KEY-BAD",
+                "can_certify": False,
+            },
+        ],
+        [("KEY-ID-OK", "RAW-KEY-OK")],
+    ),
+    (
+        [
+            {
+                "key_id": "KEY-ID-OK",
+                "raw_key": "RAW-KEY-OK",
+                "can_certify": True,
+            },
+            {
+                "key_id": "KEY-ID-OK1",
+                "raw_key": "RAW-KEY-OK1",
+                "can_certify": True,
+            },
+            {
+                "key_id": "KEY-ID-BAD",
+                "raw_key": "RAW-KEY-BAD",
+                "can_certify": False,
+            },
+        ],
+        [("KEY-ID-OK", "RAW-KEY-OK"), ("KEY-ID-OK1", "RAW-KEY-OK1")],
+    ),
+]
+
+
 @pytest.mark.parametrize(
     "response, output",
-    [
-        ([], []),
-        (
-            [
-                {
-                    "key_id": "KEY-ID-OK",
-                    "raw_key": "RAW-KEY-OK",
-                    "can_certify": True,
-                }
-            ],
-            [("KEY-ID-OK", "RAW-KEY-OK")],
-        ),
-        (
-            [
-                {
-                    "key_id": "KEY-ID-OK",
-                    "raw_key": "RAW-KEY-OK",
-                    "can_certify": True,
-                },
-                {
-                    "key_id": "KEY-ID-BAD",
-                    "raw_key": "RAW-KEY-BAD",
-                    "can_certify": False,
-                },
-            ],
-            [("KEY-ID-OK", "RAW-KEY-OK")],
-        ),
-        (
-            [
-                {
-                    "key_id": "KEY-ID-OK",
-                    "raw_key": "RAW-KEY-OK",
-                    "can_certify": True,
-                },
-                {
-                    "key_id": "KEY-ID-OK1",
-                    "raw_key": "RAW-KEY-OK1",
-                    "can_certify": True,
-                },
-                {
-                    "key_id": "KEY-ID-BAD",
-                    "raw_key": "RAW-KEY-BAD",
-                    "can_certify": False,
-                },
-            ],
-            [("KEY-ID-OK", "RAW-KEY-OK"), ("KEY-ID-OK1", "RAW-KEY-OK1")],
-        ),
-    ],
+    GITHUB_RESPONSE,
 )
 def test_parse_github_response(response, output):
     assert parse_github_response(response) == output
+
+
+@pytest.mark.parametrize(
+    "response, output",
+    GITHUB_RESPONSE,
+)
+def test_get_github_gpgs(requests_mock, response, output):
+    requests_mock.get(GITHUB_GPG_URI, request_headers=GITHUB_HEADERS, json=response)
+    assert get_github_gpgs() == output
