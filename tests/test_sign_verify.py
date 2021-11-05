4b6b442f197ad0d34dcedb0c3a11ae3a7b2fb882
ttw225 <peter86225@gmail.com>
2021-11-05 16:06:57 +0800

    test(tests/conftest.py): move `import_keys` inside conftest

--- tests/test_sign_verify.py
+++ tests/test_sign_verify.py
@@ -8,30 +8,13 @@ from validator.sign_verify import sign_text, verify_signature
 from .config import TEST_KEYS
 
 
-@pytest.fixture
-def import_keys(request):
-    sign_key, verify_key = request.param
-    if sign_key:
-        with open(f"tests/sample_keys/{sign_key['secret']}", "rb") as key_fh:
-            GPG_SIGN.import_keys(key_fh.read(), passphrase=sign_key["passphrase"])
-    if verify_key:
-        with open(f"tests/sample_keys/{verify_key['public']}", "rb") as key_fh:
-            GPG_VERIFY.import_keys(key_fh.read())
-    yield
-    if sign_key:
-        GPG_SIGN.delete_keys(sign_key["fingerprint"], secret=True, passphrase=sign_key["passphrase"])
-        GPG_SIGN.delete_keys(sign_key["fingerprint"], secret=False, passphrase=sign_key["passphrase"])
-    if verify_key:
-        GPG_VERIFY.delete_keys(verify_key["fingerprint"], secret=False)
-
-
 @pytest.mark.parametrize(
     "import_keys, sign_key, success",
     [
         ([TEST_KEYS[0], {}], TEST_KEYS[0], True),
         ([TEST_KEYS[0], {}], TEST_KEYS[1], False),
-        ([TEST_KEYS[1], {}], TEST_KEYS[1], True),
         ([TEST_KEYS[1], {}], TEST_KEYS[0], False),
+        ([TEST_KEYS[1], {}], TEST_KEYS[1], True),
     ],
     indirect=["import_keys"],
 )
