79e3f0e1844da517c2a856eefc2122d9905cdda4
ttw225 <peter86225@gmail.com>
2021-11-05 21:32:58 +0800

    test(tests/sample_keys): update key file name

--- tests/config.py
+++ tests/config.py
@@ -1,15 +1,15 @@
 TEST_KEYS = [
     {
         "id": "4BDCFAF35C34FA95",
-        "public": "example1_pub.gpg",
-        "secret": "example1_sec.gpg",
+        "public": "example1_pub.asc",
+        "secret": "example1_sec.asc",
         "fingerprint": "7D210A94B74DD63A4C3C64254BDCFAF35C34FA95",
         "passphrase": "very example p@ssword",
     },
     {
         "id": "32C5B310A13AFADA",
-        "public": "example2_pub.gpg",
-        "secret": "example2_sec.gpg",
+        "public": "example2_pub.asc",
+        "secret": "example2_sec.asc",
         "fingerprint": "25178EFFE362212489FDA16F32C5B310A13AFADA",
         "passphrase": "very example2 p@ssw0rd",
     },
