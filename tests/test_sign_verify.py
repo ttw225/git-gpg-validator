38ecbf82ba7dd086eb9ee75855adae31d4e9cdf9
ttw225 <peter86225@gmail.com>
2021-11-05 11:08:26 +0800

    test(tests/test_sign_verify.py): add test_verify_signature

--- tests/test_sign_verify.py
+++ tests/test_sign_verify.py
@@ -3,7 +3,7 @@ from typing import Dict
 import pytest
 
 from validator.config import GPG_SIGN, GPG_VERIFY
-from validator.sign_verify import sign_text
+from validator.sign_verify import sign_text, verify_signature
 
 from .config import TEST_KEYS
 
@@ -15,7 +15,7 @@ def import_keys(request):
         with open(f"tests/sample_keys/{sign_key['secret']}", "rb") as key_fh:
             GPG_SIGN.import_keys(key_fh.read())
     if verify_key:
-        with open(f"tests/sample_keys/{verify_key['secret']}", "rb") as key_fh:
+        with open(f"tests/sample_keys/{verify_key['public']}", "rb") as key_fh:
             GPG_VERIFY.import_keys(key_fh.read())
     yield
     if sign_key:
@@ -45,3 +45,22 @@ def test_sign_text(import_keys, sign_key, success):
     else:
         assert message.valid is False
         assert message.fingerprint is None
+
+
+@pytest.mark.parametrize(
+    "import_keys, sign_key, verify_key",
+    [
+        ([TEST_KEYS[0], TEST_KEYS[0]], TEST_KEYS[0], TEST_KEYS[0]),
+        ([TEST_KEYS[0], TEST_KEYS[1]], TEST_KEYS[0], TEST_KEYS[1]),
+        ([TEST_KEYS[1], TEST_KEYS[0]], TEST_KEYS[1], TEST_KEYS[0]),
+        ([TEST_KEYS[1], TEST_KEYS[1]], TEST_KEYS[1], TEST_KEYS[1]),
+    ],
+    indirect=["import_keys"],
+)
+def test_verify_signature(import_keys, sign_key, verify_key):
+    signature = sign_text(GPG_SIGN, "TEST STRING", sign_key["id"], sign_key["passphrase"])
+    key_pairs = [(verify_key["id"], open(f"tests/sample_keys/{verify_key['public']}", "rb").read())]
+    if sign_key == verify_key:
+        assert verify_signature(GPG_VERIFY, signature, key_pairs) is True
+    else:
+        assert verify_signature(GPG_VERIFY, signature, key_pairs) is False
